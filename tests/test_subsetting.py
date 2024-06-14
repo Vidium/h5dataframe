fe75e6c1ff0d723889ebf029dc72bd3cccefabf2
Matteo Bouvier <m.bouvier@vidium-solutions.com>
2024-06-14 09:44:57 +0200

    test: get column from h5df

--- tests/test_subsetting.py
+++ tests/test_subsetting.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from h5dataframe.dataframe import H5DataFrame
 
 
@@ -8,3 +9,7 @@ def test_loc(hdf5: H5DataFrame) -> None:
 
 def test_loc_multi(hdf5: H5DataFrame) -> None:
     assert np.all(hdf5.loc[[2, 1]].values == np.array([[3, "def", 3.5], [2, "bc", 2.5]], dtype=object))
+
+
+def test_get_column(hdf5: H5DataFrame) -> None:
+    assert hdf5["col_str"].equals(pd.Series(["a", "bc", "def"], name="col_str"))
